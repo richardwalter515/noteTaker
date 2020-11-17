92e211b968d89c2a9974a1f94ed0a52246318bc7
Richard Walter <richardwalter515@gmail.com>
2020-11-17 14:28:08 -0500

    updated filepath locations

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
+var http = require("http");
+const { RSA_NO_PADDING } = require("constants");
 
 
 //set up express port
@@ -9,6 +11,7 @@ var PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 
 //tell it to listen
 app.listen(PORT, function() {
@@ -19,21 +22,30 @@ app.listen(PORT, function() {
 
 // routes
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../../index.html"))
+    res.sendFile(path.join(__dirname, "/public/index.html"))
 });
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "../../notes.html"))
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+
 });
 
 //API routes:
 //get note
-// app.get("/api/notes", function(req, res) {
-//     return res.json(reservations);
-// });
+app.get("/api/notes", function(req, res) {
+    fs.readFile("/db/db.json", "utf8", function(error, data) {
+
+        if (error) {
+          return console.log(error);
+        }
+      
+        return res.json(data);
+      
+      });
+});
 // //post note
-// app.post("/api/notes", function(req, res) {
-//     return res.json(reservations);
-// });
+app.post("/api/notes", function(req, res) {
+    var newNote = req.body;
+});
 // //delete note
 // app.get("/api/notes/:id", function(req, res) {
 //     return res.json(reservations);
