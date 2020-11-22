9779edf12f39fa4b360c444e449cac73cc29531e
Richard Walter <richardwalter515@gmail.com>
2020-11-22 10:27:39 -0500

    updated Class functions

--- Develop/db/store.js
+++ Develop/db/store.js
@@ -1,32 +1,56 @@
 var fs = require("fs");
 const util = require("util");
 const uuidv1 = require("uuid");
-const express = require("express");
-const app = express();
+
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
-
-
 class Store {
-    //add functions in here.  
+    //add read and write functions in here, passing in the note in write.  
     read() {
       return readFileAsync("db/db.json", "utf8");
     };
+    write(note) {
+      return writeFileAsync("db/db.json", JSON.stringify(note));
+    }
+
     //get notes
     getNotes() {
       return this.read()
-      .then(notes => { return notes });
-      //parse and concat the notes
+      .then((notes) => { 
+        //parse and concat the notes
+        let parsedNote;
+        try {
+          parsedNote = [].concat(JSON.parse(notes));
+        } catch(err) {
+          parsedNote = [];
+        }
+        return parsedNote });
     };
+
+    //add notes
     addNotes(note) {
       const {title,text} = note;
+      //add uuid here
       const newNote  = {title, text, id: uuidv1()};
-      return this.getNotes();
+      console.log('newNote:', newNote)
+      //then return the notes with the new note
+      return this.getNotes()
+      .then((notes) => [...notes, newNote])
+      .then((updatedNotes) => this.write(updatedNotes))
+      .then(() => newNote)
     };
-    removeNotes(id) {
 
-    };
+    //delete notes
+    // removeNotes(id) {
+    // //   var id = req.params.id;
+    
+    // //   console.log(chosenNote);
+    // // };
+    // //   fs.unlink(__dirname + '/db/db.json', chosenNote, function (err) {
+    // //   if (err) throw err;
+    // //   console.log('Note deleted!');
+    // // });
 };
 
 module.exports = new Store()
--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -1,8 +1,8 @@
 const store = require("../db/store");
-var app = require("express").Router();
+const path = require("path");
+const express = require("express");
+const app = express();
 
-
-const db = [];
 // routes
 app.get("/notes", (req, res) => {
     store
@@ -16,17 +16,17 @@ app.get("/notes", (req, res) => {
 
 app.post("/api/notes", function(req, res) {
   store
-  .addNote(req.body)
+  .addNotes(req.body)
   .then((note) => res.json(note))
   .catch(err => res.status(500).json(err))
 });
 
 //delete note
-app.delete("/api/notes", function(req, res) {
-  store
-  .removeNote(req.params.id)
-  res.json(db);
-});
+// app.delete("/api/notes/:id", function(req, res) {
+//   store
+//   .removeNotes(req.params.id)
+//   res.json(db);
+// });
 
 module.exports = app;
 
@@ -36,29 +36,3 @@ module.exports = app;
   app.get("/api/notes", (req, res) => {
     store.read()
   });
-
-
-      
-      // // //delete note
-      // app.get("/api/notes/:id", function(req, res) {
-        //   var chosenNote = req.params.id;
-        
-        //   console.log(chosenNote);
-        //   fs.unlink(__dirname + '/db/db.json', chosenNote, function (err) {
-          //     if (err) throw err;
-          //     console.log('Note deleted!');
-          //   });
-          // });
-
-          
-          
-    //get note
-    // app.get("/api/notes", function(req, res) {
-    //     return fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", function(error, data) {
-    //         if (error) {
-    //           return console.log(error);
-    //         }
-    //         console.log('data:', data)
-    //         return res.json(JSON.parse(data));
-    //       });
-    // });
